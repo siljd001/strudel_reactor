81a8e930b8d2d3e26d6c34152ae112e6c45dc8f2
siljd001 <siljd001@mymail.unisa.edu.au>
2025-10-27 17:08:07 +1030

    Organised handling globalEditor inside the Processors.js

--- src/App.js
+++ src/App.js
@@ -15,13 +15,20 @@ import { stranger_tune } from "./tunes";
 import console_monkey_patch, { getD3Data } from "./console-monkey-patch";
 
 // Components
-import { ProcAndPlay, Proc, globalEditor } from "./Processors";
+import {
+  ProcAndPlay,
+  Proc,
+  setGlobalEditor,
+  getGlobalEditor,
+} from "./Processors";
 import Header from "./components/header/Header";
 import MusicInput from "./components/preprocess/MusicInput";
 import MusicProcessor from "./components/preprocess/MusicProcessor";
 import MusicPlayer from "./components/preprocess/MusicPlayer";
 import DJLivePlayer from "./components/strudel_control/DJLivePlayer";
 import DJHushers from "./components/strudel_control/DJHushers";
+
+// Global editor variable
 
 const handleD3Data = (event) => {
   console.log(event.detail);
@@ -30,21 +37,20 @@ const handleD3Data = (event) => {
 export function SetupButtons() {
   document
     .getElementById("play")
-    .addEventListener("click", () => globalEditor.evaluate());
+    .addEventListener("click", () => getGlobalEditor().evaluate());
   document
     .getElementById("stop")
-    .addEventListener("click", () => globalEditor.stop());
+    .addEventListener("click", () => getGlobalEditor().stop());
   document.getElementById("process").addEventListener("click", () => {
     Proc();
   });
   document.getElementById("process_play").addEventListener("click", () => {
-    if (globalEditor != null) {
+    if (getGlobalEditor() != null) {
       Proc();
-      globalEditor.evaluate();
+      getGlobalEditor().evaluate();
     }
   });
 }
-
 
 export default function StrudelDemo() {
   const hasRun = useRef(false);
@@ -61,30 +67,32 @@ export default function StrudelDemo() {
       canvas.height = canvas.height * 2;
       const drawContext = canvas.getContext("2d");
       const drawTime = [-2, 2]; // time window of drawn haps
-      globalEditor = new StrudelMirror({
-        defaultOutput: webaudioOutput,
-        getTime: () => getAudioContext().currentTime,
-        transpiler,
-        root: document.getElementById("editor"),
-        drawTime,
-        onDraw: (haps, time) =>
-          drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
-        prebake: async () => {
-          initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
-          const loadModules = evalScope(
-            import("@strudel/core"),
-            import("@strudel/draw"),
-            import("@strudel/mini"),
-            import("@strudel/tonal"),
-            import("@strudel/webaudio")
-          );
-          await Promise.all([
-            loadModules,
-            registerSynthSounds(),
-            registerSoundfonts(),
-          ]);
-        },
-      });
+      setGlobalEditor(
+        new StrudelMirror({
+          defaultOutput: webaudioOutput,
+          getTime: () => getAudioContext().currentTime,
+          transpiler,
+          root: document.getElementById("editor"),
+          drawTime,
+          onDraw: (haps, time) =>
+            drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
+          prebake: async () => {
+            initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
+            const loadModules = evalScope(
+              import("@strudel/core"),
+              import("@strudel/draw"),
+              import("@strudel/mini"),
+              import("@strudel/tonal"),
+              import("@strudel/webaudio")
+            );
+            await Promise.all([
+              loadModules,
+              registerSynthSounds(),
+              registerSoundfonts(),
+            ]);
+          },
+        })
+      );
 
       document.getElementById("proc").value = stranger_tune;
       SetupButtons();
@@ -119,7 +127,11 @@ export default function StrudelDemo() {
               <DJLivePlayer />
             </div>
             <div className="col-md-4">
-              <DJHushers ProcAndPlay={ProcAndPlay} musicInput={musicInput} setMusicInput={setMusicInput} />
+              <DJHushers
+                ProcAndPlay={ProcAndPlay}
+                musicInput={musicInput}
+                setMusicInput={setMusicInput}
+              />
             </div>
           </div>
         </div>
--- src/Processors.js
+++ src/Processors.js
@@ -1,16 +1,17 @@
 let globalEditor = null;
+export function setGlobalEditor(newGlobalEditor) { globalEditor = newGlobalEditor; }
+export function getGlobalEditor() { return globalEditor; }
 
 export function ProcAndPlay() {
-  if (globalEditor != null && globalEditor.repl.state.started == true) {
-    console.log(globalEditor);
+  if (getGlobalEditor() != null && getGlobalEditor().repl.state.started == true) {
+    console.log(getGlobalEditor());
     Proc();
-    globalEditor.evaluate();
+    getGlobalEditor().evaluate();
   }
 }
 
 export function Proc() {
   let proc_text = document.getElementById("proc").value;
-  globalEditor.setCode(proc_text);
+  getGlobalEditor().setCode(proc_text);
 }
 
-export { globalEditor };
